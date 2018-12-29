1af683e3d8b54575af4d2cec9703534e45dc7c3d
James Messinger <james.messinger@gmail.com>
2018-12-29 11:46:09 -0600

    Added tests for the "test" module (so meta!)

---
+++ test/specs/test.spec.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const TSLint = require("../fixtures/tslint");
+const chai = require("chai");
+chai.should();
+
+describe("tslint-modular/test", function () {
+  it("should not be enforced if module is not used", function () {
+    let results = TSLint.run(
+      "tslint-modular",
+      `
+      describe.only("My test suite", () => {
+        it.only("My test", () => {
+          return;
+        });
+      });
+      `
+    );
+
+    results.errors.should.deep.equal([]);
+    results.warnings.should.deep.equal([]);
+  });
+
+  it("should warn about describe.only() and it.only()", function () {
+    let results = TSLint.run(
+      "tslint-modular/test",
+      `
+      describe.only("My test suite", () => {
+        it.only("My test", () => {
+          return;
+        });
+      });
+      `
+    );
+
+    results.errors.should.deep.equal([]);
+
+    results.warnings.should.deep.equal([
+      "ban",
+      "ban",
+    ]);
+
+    results.warningMessages.should.deep.equal([
+      "Calls to 'describe.only' are not allowed. Don't forget to remove this .only",
+      "Calls to 'it.only' are not allowed. Don't forget to remove this .only",
+    ]);
+  });
+});
