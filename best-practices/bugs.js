efd054f0568dbc6ae34000ad34447265afef271c
James Messinger <james.messinger@gmail.com>
2019-01-01 05:18:07 -0600

    Disabled the "strict-type-predicates" rule because it's often necessary to add `typeof` checks that TypeScript thinks are unnecessary

--- best-practices/bugs.js
+++ best-practices/bugs.js
@@ -280,21 +280,27 @@ module.exports = {
      *   - Conditions for `if`, `for`, `while`, and `do-while` statements.
      *
      * @see https://palantir.github.io/tslint/rules/strict-boolean-expressions/
+     *
+     * This rule is disabled because truthy and falsy assertions are a common and well-accepted
+     * practice in JavaScript.
      */
-    "strict-boolean-expressions": true,
+    "strict-boolean-expressions": false,
 
     /**
      * Warns for type predicates that are always true or always false.
-     * Works for `typeof` comparisons to constants (e.g. `typeof foo === “string”`), and equality comparison to `null`/`undefined`.
-     * (TypeScript won`t let you compare `1 === 2`, but it has an exception for `1 === undefined`.)
+     * Works for `typeof` comparisons to constants (e.g. `typeof foo === "string"`), and equality comparison to `null`/`undefined`.
+     * (TypeScript won't let you compare `1 === 2`, but it has an exception for `1 === undefined`.)
      * Does not yet work for `instanceof`.
      * Does not warn for `if (x.y)` where `x.y` is always truthy. For that, see strict-boolean-expressions.
      *
      * This rule requires `strictNullChecks` to work properly.
      *
      * @see https://palantir.github.io/tslint/rules/strict-type-predicates/
+     *
+     * This rule is disabled because it's often necessary to add `typeof` checks that the TypeScript
+     * compiler thinks are unnecessary.
      */
-    "strict-type-predicates": true,
+    "strict-type-predicates": false,
 
     /**
      * Require a `default` case in all `switch` statements.
